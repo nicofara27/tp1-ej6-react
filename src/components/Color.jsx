10cc7a60f3b2eb7daf60d70a417fa02173701f63
Nicolas Fara <nicolas.fara.romero@gmail.com>
2022-09-28 20:51:42 -0300

    Ejercicio finalizado

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -1,21 +1,29 @@
 import { Card, Button } from "react-bootstrap";
 
-const Color = () => {
+const Color = ({key, nombreColor, borrarColor }) => {
   return (
-    <article className="my-2">
+    <article className="m-2 col-8 col-md-3 col-lg-2">
       <Card>
         <Card.Body className="px-0">
-          <Card.Title className="px-5">Color nombre</Card.Title>
-          <div className="bg-celeste px-5 py-3">
-            <div className="py-5" style={{background:"blue"}}></div>
+          <Card.Title className="ms-3 lead fw-bolder fs-4">{nombreColor}</Card.Title>
+          <div className="bg-celeste px-5 px-md-4 py-3">
+            <div className="py-5" style={{ background: nombreColor }}></div>
           </div>
           <div className="text-end mt-3 me-3">
-          <Button className="shadow" variant="danger">Borrar</Button>
+            <Button
+              className="shadow"
+              variant="danger"
+              onClick={() => {
+                borrarColor(nombreColor);
+              }}
+            >
+              Borrar
+            </Button>
           </div>
         </Card.Body>
       </Card>
     </article>
-  );
-};
+  )
+}
 
 export default Color;
--- src/components/FormularioColores.jsx
+++ src/components/FormularioColores.jsx
@@ -1,23 +1,45 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import Color from "./Color";
 
 const FormularioColores = () => {
+  const coloresLocalStorage = JSON.parse(localStorage.getItem('listaColores')) || [];
+  const [color, setColor] = useState("");
+  const [arregloColores, setArregloColores] = useState(coloresLocalStorage);
+
+  useEffect (()=> {
+    localStorage.setItem("listaColores", JSON.stringify(arregloColores))
+  })
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    setArregloColores([...arregloColores, color]);
+    setColor("");
+  }
+
+  const borrarColor = (color) => {
+    let arregloMod = arregloColores.filter((item) => item !== color);
+
+    setArregloColores(arregloMod);
+  }
+
   return (
     <>
       <section className="d-flex flex-wrap align-items-center justify-content-around bg-celeste py-3">
         <div
           className="py-5 mb-4 mb-md-0 col-4 col-md-2 col-lg-1"
-          style={{ background: "blue" }}
+          style={{ background: color }}
         ></div>
         <div className="col-10 col-md-8 col-lg-10">
-          <Form className="d-flex align-items-center">
+          <Form className="d-flex align-items-center" onSubmit={handleSubmit}>
             <Form.Group className="w-100" controlId="formBasicEmail">
               <Form.Control
                 className="text-decoration-underline"
                 type="text"
                 placeholder="Ingrese un color ej blue"
-                //   onChange={(e) => setTarea(e.target.value)}
-                //   value={tarea}
+                onChange={(e)=> setColor(e.target.value)}
+                value={color}
               />
             </Form.Group>
             <Button className="shadow" variant="info" type="submit">
@@ -27,9 +49,9 @@ const FormularioColores = () => {
         </div>
       </section>
       <section className="d-flex justify-content-center justify-content-md-around flex-wrap mt-5">
-      <Color></Color>
-      <Color></Color>
-      <Color></Color>
+        {arregloColores.map((color, posicion) => (
+          <Color key={posicion} nombreColor={color} borrarColor={borrarColor}></Color>
+        ))}
       </section>
     </>
   );
